af0ecd8b05602f012e2f879334234985d508d549
AGStha <gongmakz.anuj@gmail.com>
2018-10-27 12:12:04 +0545

    Frrstday.ipynb was changed . List variables, strings were added

---
+++ Strings variables and integer.py
@@ -0,0 +1,44 @@
+a=10
+print (a)
+
+b=a+10
+print (b)
+
+print (a**2) #it shows square of a
+
+print("hello"*2) #it multiplies the string twice
+
+print (4/10)
+ #multiple assignment
+
+a, b, c = 1, 2, "john"
+print (a,b,c)
+
+a = b = c = 1
+print (a)
+
+a=b=c=20
+print (a,b,c)
+
+'''Standard Data Types
+
+The data stored in memory can be of many types. For example, a person's age is stored as a numeric value and his or her address is stored as alphanumeric characters. Python has various standard data types that are used to define the operations possible on them and the storage method for each of them.
+
+Python has five standard data types −
+
+    Numbers
+    String
+    List
+    Tuple
+    Dictionary
+'''
+#Assigning variable and deleting variable
+a=10
+del a
+'''python supports following number types
+int
+float
+long
+complec
+'''
+#Python Strings
