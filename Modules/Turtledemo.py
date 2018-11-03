1355e48584c0a42dcb2b52c3431ced16eb63515e
AGStha <gongmakz.anuj@gmail.com>
2018-11-03 09:03:46 +0545

    Module Directory was added

---
+++ Modules/Turtledemo.py
@@ -0,0 +1,14 @@
+#This is the simple program to learn importing the python module
+import turtle
+import time
+
+turtle.forward(200)
+turtle.right(300)
+turtle.forward(202)
+time.sleep(5)
+'''from turtle import forward,right,circle
+forward(200)
+right(200)
+circle(200)
+'''
+#import turtle*
