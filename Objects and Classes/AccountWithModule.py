2f0a30b62f1473594ca04c8020e66d4fcee6872d
AGStha <gongmakz.anuj@gmail.com>
2018-11-07 17:57:10 +0545

    NON-Public method and mangling of the class attributes using _

--- Objects and Classes/AccountWithModule.py
+++ Objects and Classes/AccountWithModule.py
@@ -1,6 +1,7 @@
 import datetime
 import pytz
 
+
 class Account:
 
     @staticmethod               #we can make static method by removing self and adding @staticmethod.
@@ -8,40 +9,58 @@ class Account:
         utc_time=datetime.datetime.utcnow()
         return pytz.utc.localize(utc_time)
 
+    def __init__(self, _name, _balance):
+        self._name = _name      # we use _name to make it private or non-public
 
-    def __init__(self,name,balance):
-        self.name=name
-        self.balance=balance
-        self.trasaction_list=[]
+        self._balance = _balance
+        self.trasaction_list = [(Account._current_time(), _balance)]
+        print("Account Created for {}".format(_name))
+        self.show_balance()
 
-    def deposit(self,amt):
-        self.balance+=amt
+    def deposit(self , amt):
+        self._balance+=amt
         #self.trasaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()),amt))
         self.trasaction_list.append((Account._current_time(),amt))#Static method call of class Account.
-
         self.show_balance()
+
     def withdraw(self,amt):
-        if self.balance>amt>0:
-            self.balance-=amt
+        if self._balance>amt>0:
+            self._balance-=amt
             self.show_balance()
-            self.trasaction_list.append((Account._current_time(),-amt))
+            self.trasaction_list.append((Account._current_time(), -amt))
         else:
             print("Insufficient Amount")
             self.show_balance()
+
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self._balance))
+
     def show_transaction(self):
         for date,amt in self.trasaction_list:
             if amt>0:
-                tran_type="Deposited"
+                tran_type = "Deposited"
             else:
-                tran_type="Withdrawn"
+                tran_type = "Withdrawn"
                 amt*-1
-            print("{} {} on {} (local time was {})".format(amt,tran_type,date,date.astimezone()))
+            print("{} {} on {} (local time was {})".format(amt, tran_type, date, date.astimezone()))
 
-if __name__== '__main__':
-    anuj=Account('Anuj',100)
+
+if __name__ == '__main__':
+    bibi= Account('Bibika',0)
+    bibi.deposit(100)
+    bibi.withdraw(50)
+    bibi.show_transaction()
+
+    anuj = Account('Anuj', 100)
+    anuj._balance=200        # We should not allow client outside class to add like this.
+    #So we use '_' underscore to protect it.in the declaration part.
+    #but we can use
+    anuj.__balance=200 # we can use this to access the attribute again.
     anuj.deposit(50)
-    anuj.withdraw(200)
+    anuj.withdraw(90)
     anuj.show_transaction()
+    anuj.show_balance()
+    print(anuj.__dict__)
+    anuj._Account__balance=200#This is discouraged.
+    anuj.show_balance()
 
