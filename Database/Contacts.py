0973c421292cf92a5e2ffc29115aee702b7fb1d2
AGStha <gongmakz.anuj@gmail.com>
2018-11-23 19:35:07 +0545

    Createtable was renamed as contacts and contacts2 was also added

--- Database/Contacts.py
+++ Database/Contacts.py
@@ -11,8 +11,24 @@ db.execute("Insert into contacts values ('Andrew', 2, 'juk@gmail.com')")
 #We can display through cursor function.
 cursor = db.cursor()
 cursor.execute("select *from contacts")
-for row in cursor:
+"""for row in cursor:
     print(row)
+"""
+#cursor is the generator type of function.
+"""
+#fetchall method in cursor function which adds into the list so the generator doesnt work
 
+#print(cursor.fetchall())
+
+#Fetch One is used to pull single row.
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+"""
+for name,phone,email in cursor:
+    print(name)
+    print(phone)
+    print(email)
+    print('-'*10)
 cursor.close()
 db.close()
