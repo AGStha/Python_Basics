52b86c0e5f752d6f772513e805f7aa30eaae68e2
AGStha <gongmakz.anuj@gmail.com>
2018-11-23 19:24:15 +0545

    Createtable.py was edited as cursor function added.

--- Database/Createtable.py
+++ Database/Createtable.py
@@ -2,8 +2,17 @@ import sqlite3
 
 
 db = sqlite3.connect("contacts.sqlite")
-db.execute("CREATE TABLE contacts (name text, phone int, email text)")
+
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INT, email TEXT)")
 db.execute("INSERT INTO contacts (name,phone,email) VALUES ('Anuj',22,'anuj@gmail.com')")
 db.execute("Insert into contacts values ('Andrew', 2, 'juk@gmail.com')")
 
+
+#We can display through cursor function.
+cursor = db.cursor()
+cursor.execute("select *from contacts")
+for row in cursor:
+    print(row)
+
+cursor.close()
 db.close()
