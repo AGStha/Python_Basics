cd06d828db77e4f2dae5d0b91e0005bfaa287793
AGStha <gongmakz.anuj@gmail.com>
2018-11-23 20:51:16 +0545

    Sql injection testing learning

--- Database/Contacts2.py
+++ Database/Contacts2.py
@@ -5,7 +5,14 @@ db = sqlite3.connect("contacts.sqlite")
 #this is alt method to call the python sql without cursor function.
 
 #Update using cursor
-update_sql = "update contacts set email='changed@gmail.com' where contacts.name='Anuj'"
+
+#update_sql = "update contacts set email='changed@gmail.com' where contacts.name='Anuj'"
+
+#Using placeholder method/ pass through user input.
+email='sqlinject@gmail.com'
+phone=input("enter the phone")
+update_sql = "update contacts set email='{}' where phone={}".format(email,phone)
+
 
 update_cursor = db.cursor()
 update_cursor.execute(update_sql)
--- Database/checkdb.py
+++ Database/checkdb.py
@@ -1,4 +1,4 @@
-import  sqlite3
+import sqlite3
 
 
 con = sqlite3.connect("contacts.sqlite")
