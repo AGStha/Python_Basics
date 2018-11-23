0a02e7c73998da3d73e44f9623c2002f61b48518
AGStha <gongmakz.anuj@gmail.com>
2018-11-23 20:23:56 +0545

    checkdb.py was created which checks if the commit happened in contact2.py file.

--- Database/Contacts2.py
+++ Database/Contacts2.py
@@ -5,11 +5,17 @@ db = sqlite3.connect("contacts.sqlite")
 #this is alt method to call the python sql without cursor function.
 
 #Update using cursor
-update_sql = "update contacts set email='changed@gmail.com' where contacts.name='Anuja'"
+update_sql = "update contacts set email='changed@gmail.com' where contacts.name='Anuj'"
 
 update_cursor = db.cursor()
 update_cursor.execute(update_sql)
 print("{} rows updated".format(update_cursor.rowcount))
+
+print()
+print("Are the connection same: {}".format(update_cursor.connection == db))
+print()
+
+update_cursor.connection.commit()
 update_cursor.close()
 for name, phone, email in db.execute("select *from contacts"):
     print(name)
---
+++ Database/checkdb.py
@@ -0,0 +1,8 @@
+import  sqlite3
+
+
+con = sqlite3.connect("contacts.sqlite")
+for row in con.execute("select * from contacts"):
+    print(row)
+
+con.close()
