3f2784c679ed6a3315534bbf4bc50d23dfeeff90
AGStha <gongmakz.anuj@gmail.com>
2018-11-24 18:39:00 +0545

    placehoder and parameter substitution learned in checkdb.py

--- Database/checkdb.py
+++ Database/checkdb.py
@@ -3,9 +3,14 @@ import sqlite3
 
 con = sqlite3.connect("contacts.sqlite")
 #We can check the schema. and attack by followin
-
-#for row in con.execute("select *from sqlite_master"):
-for row in con.execute("select * from contacts"):
+name=input("Enter the name")
+#con=sqlite3.connect("contacts.sqlite3")
+#for row in con.execute("select * from contacts where name=?",(name,)):
+for row in con.execute("select * from contacts where name like ?", (name,)):  #(name,) Parameter sustitution.and ? is place holder
     print(row)
 
+#for  row in con.execute("select *from sqlite_master"):
+"""for row in con.execute("select * from contacts"):
+    print(row)
+"""
 con.close()
