a180858e10d8f4d978d761ed013d6f117f928a36
AGStha <gongmakz.anuj@gmail.com>
2018-11-28 17:26:17 +0545

    Sample code shown to brother.

--- Database/Rollback/rollback.py
+++ Database/Rollback/rollback.py
@@ -1,3 +1,13 @@
+import sqlite3
+
+
+db = sqlite3.connect("accounts.sqlite")
+db.execute("create table if not exists accounts (name text primary key not null,"
+           " balance integer not null")
+db.execute("create table if not exists transactions (time timestamp not null,"
+           " account text not null, amount integer not null, primary key (time, account)) ")
+
+
 class Account(object):
     def __init__(self, name: str, opening_balance: float = 0.0):
         self.name = name
@@ -8,28 +18,28 @@ class Account(object):
     def deposit(self, amount: float) -> float:
         if amount > 0.0:
             self._balance += amount
-            print("{} deposited on {} Account".format(amount, self.name))
+            print("{} deposited on {} Account".format(amount/100, self.name))
         else:
             print("Balance is negative")
-        return self._balance
+        return self._balance/100
 
     def withdraw(self, amount: float) -> float:
         if 0.0 < amount < self._balance:
             self._balance -= amount
             print("{} withdrwawn from {}".format(amount, self.name))
-            return amount
+            return amount/100
         else:
             print("amount should be lesser than current balance and greater than 0 ")
             return 0.0
 
     def show_balance(self):
-        print("Balance for {} is {} ".format(self.name, self._balance))
+        print("Balance for {} is {} ".format(self.name, self._balance/100))
 
 
 if __name__ == '__main__':
     acc = Account("Anuj")
-    acc.deposit(20.0)
-    acc.deposit(10.0)
-    acc.withdraw(15.0)
-    acc.withdraw(0.0)
+    acc.deposit(2000)
+    acc.deposit(1000)
+    acc.withdraw(1500)
+    acc.withdraw(0)
     acc.show_balance()
